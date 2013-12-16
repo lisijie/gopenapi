3db2adb2c5c1425f3f642242b38e414740fa2ab3
lisijie <Lsijie@Lsijie-PC.(none)>
2013-12-16 15:59:35 +0800

    用户相关api

---
+++ types.go
@@ -0,0 +1,81 @@
+package gopenapi
+
+type RetBase struct {
+	Ret     int
+	Msg     string
+	Is_Lost int
+}
+
+//GetInfo() 返回的用户信息结构
+type UserInfo struct {
+	RetBase
+	Nickname           string //昵称
+	Gender             string //性别
+	Country            string //国家（当pf=qzone、pengyou或qplus时返回）
+	Province           string //省（当pf=qzone、pengyou或qplus时返回）
+	City               string //市（当pf=qzone、pengyou或qplus时返回）
+	Figureurl          string //头像URL
+	Openid             string //用户QQ号码转化得到的ID（当pf=qplus时返回）
+	Qq_level           int    //用户QQ等级（当pf=qplus时返回）
+	Qq_vip_level       int    //用户QQ会员等级（当pf=qplus时返回）
+	Qplus_level        int    //用户Q+等级（当pf=qplus时返回）
+	Is_yellow_vip      int    //是否为黄钻用户（0：不是； 1：是）
+	Is_yellow_year_vip int    //是否为年费黄钻用户（0：不是； 1：是）
+	Yellow_vip_level   int    //黄钻等级，目前最高级别为黄钻8级（如果是黄钻用户才返回此参数）
+	Is_yellow_high_vip int    //是否为豪华版黄钻用户（0：不是； 1：是）
+	Is_blue_vip        int    //是否为蓝钻用户（0：不是； 1：是）
+	Is_blue_year_vip   int    //是否为年费蓝钻用户（0：不是； 1：是）
+	Blue_vip_level     int    //蓝钻等级（如果是蓝钻用户才返回此参数）
+	Is_super_blue_vip  int    //是否是豪华蓝钻
+}
+
+//TotalVipInfo() 返回的用户VIP信息
+type UserVipInfo struct {
+	RetBase
+	//QQ会员
+	Is_vip      int //是否为QQ会员（0：不是； 1：是）
+	Is_Year_Vip int //是否是年费QQ会员（0：不是； 1：是）
+	Vip_level   int //QQ会员等级
+	Is_high_vip int //是否豪华版QQ会员（0：不是； 1：是）
+	//蓝钻
+	Is_Blue      int //是否为蓝钻用户（0：不是； 1：是）
+	Is_year_blue int //是否是年费蓝钻用户（0：不是； 1：是）
+	Blue_level   int //蓝钻等级
+	Is_high_blue int //是否豪华版蓝钻（0：不是； 1：是）
+	//黄钻
+	Is_yellow         int //是否为黄钻用户（0：不是； 1：是）
+	Is_year_yellow    int //是否是年费黄钻用户（0：不是； 1：是）
+	Yellow_level      int //黄钻等级
+	Is_high_yellow    int //是否豪华版黄钻（0：不是； 1：是）
+	Is_gamevip_yellow int //是否为超级套餐包月（0：不是； 1：是）
+	//红钻
+	Is_red      int //是否为红钻用户（0：不是； 1：是）
+	Red_level   int //红钻等级
+	Is_high_red int //是否豪华版红钻（0：不是； 1：是）
+	//绿钻
+	Is_green      int //是否为绿钻用户（0：不是； 1：是）
+	Is_year_green int //是否是年费绿钻用户（0：不是； 1：是）
+	Green_level   int //绿钻等级
+	Is_high_green int //是否豪华绿钻（0：不是； 1：是）
+	//粉钻
+	Is_pink         int //是否为粉钻用户（0：不是； 1：是）
+	Is_year_pink    int //是否是年费粉钻用户（0：不是； 1：是）
+	Pink_level      int //粉钻等级
+	Is_pink_15      int //是否是15元粉钻用户（0：不是； 1：是）
+	Is_year_pink_15 int //是否是年费15元粉钻用户（0：不是； 1：是）
+	//超级qq
+	Is_superqq    int //是否为超级QQ用户（0：不是； 1：是）
+	Superqq_level int //超级QQ等级
+	//3366
+	Level_3366 int //3366会员等级
+}
+
+//IsVip() 返回的用户VIP信息
+type UserIsVip struct {
+	RetBase
+	Is_yellow_vip      int //是否为黄钻用户（0：不是； 1：是）
+	Is_yellow_year_vip int //是否为年费黄钻用户（0：不是； 1：是）
+	Yellow_vip_level   int //黄钻等级。目前最高级别为黄钻8级（如果是黄钻用户才返回此字段）
+	Is_yellow_high_vip int //是否为豪华版黄钻用户（0：不是； 1：是）
+	Yellow_vip_pay_way int //用户的付费类型
+}
--- user.go
+++ user.go
@@ -1,6 +1,8 @@
 package gopenapi
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
 )
 
@@ -8,6 +10,7 @@ type User struct {
 	openid  string
 	openkey string
 	pf      string
+	charset string
 	api     *OpenApi
 }
 
@@ -19,20 +22,134 @@ func (this *User) SetApi(api *OpenApi) {
 	this.api = api
 }
 
-func (this *User) makeParams() map[string]string {
+func (this *User) makeParams(p map[string]string) map[string]string {
 	params := make(map[string]string)
 	params["openid"] = this.openid
 	params["openkey"] = this.openkey
 	params["pf"] = this.pf
+	for k, v := range p {
+		params[k] = v
+	}
 	return params
 }
 
-func (this *User) GetInfo() {
-	params := this.makeParams()
-	ret, err := this.api.Api("/v3/user/get_info", params, "post", "http")
+/**
+ * 获取登录用户的信息，包括昵称、头像、性别等信息
+ * @see http://wiki.open.qq.com/wiki/v3/user/get_info
+ */
+func (this *User) GetInfo(p map[string]string) (UserInfo, error) {
+	var data UserInfo
+	ret, err := this.api.Api("/v3/user/get_info", this.makeParams(p), "post", "http")
 	if err != nil {
 		log.Fatal(err)
+		return data, err
 	} else {
-		log.Println(string(ret))
+		json.Unmarshal(ret, &data)
+		if data.Ret > 0 {
+			return data, errors.New(data.Msg)
+		}
+		return data, nil
+	}
+}
+
+/**
+ * 获取登录用户的VIP信息
+ * @see http://wiki.open.qq.com/wiki/v3/user/total_vip_info
+ */
+func (this *User) TotalVipInfo(p map[string]string) (UserVipInfo, error) {
+	var data UserVipInfo
+	ret, err := this.api.Api("/v3/user/total_vip_info", this.makeParams(p), "post", "http")
+	if err != nil {
+		log.Fatal(err)
+		return data, err
+	} else {
+		json.Unmarshal(ret, &data)
+		if data.Ret > 0 {
+			return data, errors.New(data.Msg)
+		}
+		return data, nil
+	}
+}
+
+/**
+ * 验证登录用户是否黄钻，是否年费黄钻，如果是则返回其黄钻等级等信息。
+ * @see http://wiki.open.qq.com/wiki/v3/user/is_vip
+ */
+func (this *User) IsVip() (UserIsVip, error) {
+	var data UserIsVip
+	ret, err := this.api.Api("/v3/user/is_vip", this.makeParams(nil), "post", "http")
+	if err != nil {
+		log.Fatal(err)
+		return data, err
+	} else {
+		json.Unmarshal(ret, &data)
+		if data.Ret > 0 {
+			return data, errors.New(data.Msg)
+		}
+		return data, nil
+	}
+}
+
+/**
+ * 验证登录用户是否安装了应用
+ * @see http://wiki.open.qq.com/wiki/v3/user/is_setup
+ */
+func (this *User) IsSetup() (bool, error) {
+	ret, err := this.api.Api("/v3/user/is_setup", this.makeParams(nil), "post", "http")
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	} else {
+		data := make(map[string]interface{})
+		json.Unmarshal(ret, &data)
+
+		if data["ret"] == 0.0 {
+			return data["setuped"] == 1.0, nil
+		} else {
+			return false, errors.New("error")
+		}
+	}
+}
+
+/**
+ * 验证用户的登录态，判断openkey是否过期，没有过期则对openkey有效期进行续期（一次调用续期2小时）。
+ * @see http://wiki.open.qq.com/wiki/v3/user/is_login
+ */
+func (this *User) IsLogin() (bool, error) {
+	ret, err := this.api.Api("/v3/user/is_login", this.makeParams(nil), "post", "http")
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	} else {
+		var data RetBase
+		json.Unmarshal(ret, &data)
+		if data.Ret > 0 {
+			return false, errors.New(data.Msg)
+		} else {
+			return true, nil
+		}
+	}
+}
+
+/**
+ * 本接口仅适用于多区多服应用，用来验证用户登录态（即验证openkey），以及验证用户是否从选区页面（即验证seqid）进入应用。
+ * 多区多服应用中设置有验证用户是否从选区页面进入应用的逻辑，将有助于防止用户直接通过修改应用地址的方式进入应用。
+ * @see http://wiki.open.qq.com/wiki/v3/user/is_area_login
+ */
+func (this *User) IsAreaLogin(seqid string) (bool, error) {
+	p := make(map[string]string)
+	p["seqid"] = seqid
+	ret, err := this.api.Api("/v3/user/is_area_login", this.makeParams(p), "post", "http")
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	} else {
+		var data RetBase
+		json.Unmarshal(ret, &data)
+		if data.Ret > 0 {
+			return false, errors.New(data.Msg)
+		} else {
+			return true, nil
+		}
 	}
 }
